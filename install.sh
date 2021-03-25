d5645190033e4b979bb4345b45a658e5b30063f4
Benjamin Arntzen <zorlin@gmail.com>
2021-03-25 16:29:21 +0800

    Fix a few things and disable mitogen :(

--- install.sh
+++ install.sh
@@ -5,9 +5,13 @@ brew install wget
 sudo -H pip3 install ansible molecule wget
 # Download mitogen
 sudo wget -nc -c "https://github.com/mitogen-hq/mitogen/archive/v0.3.0-rc.0.zip" -O /opt/v0.3.0-rc.0.zip
-mkdir -p /opt/mitogen
+sudo mkdir -p /opt/mitogen
 sudo bsdtar --strip-components=1 -xvf /opt/v0.3.0-rc.0.zip -C /opt/mitogen
+# Generate a pubkey
+echo TODO - keypair
+# Add our pubkey
+ssh-add wings@localhost
 # Scan the local key
-sudo ssh-keyscan -H localhost >> ~/.ssh/known_hosts
+ssh-keyscan -H localhost >> ~/.ssh/known_hosts
 # Grab the Ansible collections and roles
 ansible-galaxy install -r roles/requirements.yml
